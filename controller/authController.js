bd311ef1be3ae7664032a5e88a208a94b050a6ad
Aqsa <cendekiaqsa@gmail.com>
2024-12-26 23:04:20 +0700

    fixed

--- controller/authController.js
+++ controller/authController.js
@@ -7,26 +7,26 @@ const upload = multer();
 
 const registerUser = async (req, res) => {
     upload.none()(req, res, async function () {
-    const { name, email, password, phone, role_id } = req.body;
-    if (!name || !email || !password || !phone) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'All fields are required'
-        });
-    }
+        const { name, email, password, phone, role_id } = req.body;
+        if (!name || !email || !password || !phone) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'All fields are required'
+            });
+        }
 
-    try {
-        await authModel.registerUser(name, email, password, phone, role_id);
-        res.status(201).json({
-            status: 'success',
-            message: 'User registered successfully'
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err.message || 'Server error during registration'
-        });
-    }
+        try {
+            await authModel.registerUser(name, email, password, phone, role_id);
+            res.status(201).json({
+                status: 'success',
+                message: 'User registered successfully'
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: 'error',
+                message: err.message || 'Server error during registration'
+            });
+        }
     })
 
 };
@@ -62,42 +62,42 @@ const updateUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     upload.none()(req, res, async function () {
-    const { email, password } = req.body;
+        const { email, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'All fields are required'
-        });
-    }
-
-    try {
-        const { user, isMatch } = await authModel.loginUser(email, password);
-
-        if (!user || !isMatch) {
-            return res.status(401).json({
+        if (!email || !password) {
+            return res.status(400).json({
                 status: 'error',
-                message: 'Invalid credentials'
+                message: 'All fields are required'
             });
         }
 
-        const token = jwt.sign({ id: user.id, role_id: user.role_id }, JWT_SECRET);
-        res.status(200).json({
-            status: 'success',
-            message: 'User logged in successfully',
+        try {
+            const { user, isMatch } = await authModel.loginUser(email, password);
+
+            if (!user || !isMatch) {
+                return res.status(401).json({
+                    status: 'error',
+                    message: 'Invalid credentials'
+                });
+            }
+
+            const token = jwt.sign({ id: user.id, role_id: user.role_id }, JWT_SECRET);
+            res.status(200).json({
+                status: 'success',
+                message: 'User logged in successfully',
                 token: token,
                 data: {
                     id: user.user_id,
                     name: user.name,
                     email: user.email
                 }
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err.message || 'Server error during login',
-        });
-    }
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: 'error',
+                message: err.message || 'Server error during login',
+            });
+        }
     });
 };
 
